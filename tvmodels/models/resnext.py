4bf4dd823369ae793e0f5298e28766b947fde069
rohitgr7 <rohitgr1998@gmail.com>
2019-07-04 21:02:50 +0530

    Add ResNext WSL models

--- tvmodels/models/resnext.py
+++ tvmodels/models/resnext.py
@@ -7,7 +7,10 @@ __all__ = ['resnext50_32x4', 'resnext101_32x4',
 META = {
     'resnext50_32x4': ['resnext50_32x4.pth', 'https://drive.google.com/open?id=1NOni3ClRTaO2cUB-wRSm69qZye66CvPd'],
     'resnext101_32x4': ['resnext101_32x4.pth', 'https://drive.google.com/open?id=1flTAweS7XpnmduRe714266PPP_W-WyUM'],
-    'resnext101_32x8': ['resnext101_32x8.pth', 'https://drive.google.com/open?id=1RhPki5kNIGUsBL8YY7uBo5KGCQwgkD02'],
+    'resnext101_32x8': ['resnext101_32x8.pth', 'https://drive.google.com/open?id=1-G1X-tQvjil7UJhvqoQhhQgyOfutaGkg'],
+    'resnext101_32x16': ['resnext101_32x16.pth', 'https://drive.google.com/open?id=1-B29ewr2bJRn7XMfsH2SuA96xgviLMTE'],
+    'resnext101_32x32': ['resnext101_32x32.pth', 'https://drive.google.com/open?id=1-1YB417ZIwbifHUTB4mIn9Re4ofoVOmq'],
+    'resnext101_32x48': ['resnext101_32x48.pth', 'https://drive.google.com/open?id=1-18aLEcE0WigP449LiN4xWl9EWuWW8uj'],
     'resnext101_64x4': ['resnext101_64x4.pth', 'https://drive.google.com/open?id=1B3OsbSaQlD5I3IhexHNbTfZ8n6EibSkZ'],
     'resnext152': []
 }
@@ -26,6 +29,21 @@ def resnext101_32x4(nc=1000, pretrained=False, dest=None):
 def resnext101_32x8(nc=1000, pretrained=False, dest=None):
     m = ResNet(ResNetBlock, [3, 4, 23, 3], nc=nc, groups=32, base_width=8)
     return load_pretrained(m, META['resnext101_32x8'], dest, pretrained)
+
+
+def resnext101_32x16(nc=1000, pretrained=False, dest=None):
+    m = ResNet(ResNetBlock, [3, 4, 23, 3], nc=nc, groups=32, base_width=16)
+    return load_pretrained(m, META['resnext101_32x16'], dest, pretrained)
+
+
+def resnext101_32x32(nc=1000, pretrained=False, dest=None):
+    m = ResNet(ResNetBlock, [3, 4, 23, 3], nc=nc, groups=32, base_width=32)
+    return load_pretrained(m, META['resnext101_32x32'], dest, pretrained)
+
+
+def resnext101_32x48(nc=1000, pretrained=False, dest=None):
+    m = ResNet(ResNetBlock, [3, 4, 23, 3], nc=nc, groups=32, base_width=48)
+    return load_pretrained(m, META['resnext101_32x48'], dest, pretrained)
 
 
 def resnext101_64x4(nc=1000, pretrained=False, dest=None):
